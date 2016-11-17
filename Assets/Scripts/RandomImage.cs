af662b5c41d48709ebba1283d018b530af40c2bb
Brad Gearon <bradgearon@gmail.com>
2016-11-16 21:53:54 -0600

    added random image

---
+++ Assets/Scripts/RandomImage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RandomImage : MonoBehaviour
+{
+    public Sprite[] Images;
+
+    private Image image;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    image = GetComponent<Image>();
+	    var imageIndex = Random.Range(0, Images.Length);
+	    image.sprite = Images[imageIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
