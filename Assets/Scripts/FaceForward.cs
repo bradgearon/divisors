6fe66209f48085c80c261c2357d8f7451a4a9b1b
Brad Gearon <bradgearon@gmail.com>
2016-11-17 22:39:22 -0600

    flipping the animal

---
+++ Assets/Scripts/FaceForward.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FaceForward : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(showImage());
+    }
+
+    private IEnumerator showImage()
+    {
+        var image = GetComponent<Image>();
+        yield return new WaitForSeconds(.15f);
+        yield return image.transform.DORotate(Vector3.zero, .25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
